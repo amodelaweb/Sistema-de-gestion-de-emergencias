6c34c13736cf2ce823859b81c5fbf717d487a5ec
Juan Pablo Penaloza <juan-penaloza@javeriana.edu.co>
2018-09-13 12:11:04 -0500

    Start setup

--- Publicador/Modelo/Publisher.py
+++ Publicador/Modelo/Publisher.py
@@ -1,11 +1,21 @@
-from ... import Noticia
+import sys
+import os
+
+from ..Noticia import Noticia
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 class Publisher:
     """This class is goning to send news to the broker."""
-    def __init__(self, ip, port, broker):
+    def __init__(self, ip, port, broker=None):
         self.ip = ip
         self.port = port
         self.broker = broker
 
     def send_news(self):
         news = Noticia(body='Test Body', author='El Tiempo')
+
+    def imprimir(self):
+        print(self.ip);
+        print(self.port);
+
+print('Hello World')
