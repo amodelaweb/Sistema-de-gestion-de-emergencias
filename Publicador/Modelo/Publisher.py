314b316e5ce4ae1e3f0da7ea58373542d43369d9
Juan Pablo Penaloza <juan-penaloza@javeriana.edu.co>
2018-09-13 18:23:29 -0500

    edited news and finished publisher

--- Publicador/Modelo/Publisher.py
+++ Publicador/Modelo/Publisher.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 
 sys.path.insert(1, os.path.join(sys.path[0], '../..'))
 from shared.Noticia import Noticia
@@ -9,17 +10,17 @@ from shared.Connection import Connection
 
 class Publisher:
     """This class is goning to send news to the broker."""
-    def __init__(self, ip_broker ,nombre, ip='127.0.0.1', port=5001, port_broker=5001):
+    def __init__(self, ip_broker, nombre, ip='127.0.0.1', port=5001, port_broker=5001):
         self.ip = ip
         self.port = port
-        self.brokerPort = brokerPort
-        self.brokerIp = brokerIp
+        self.port_broker = port_broker
+        self.ip_broker = ip_broker
         self.nombre = nombre
 
     def send_news(self, news):
         msg = Message(MessageType.NEWS, json.loads(news.to_json()))
-        conn = Conection();
-        conn.send_data(port=self.brokerPort,  ip=self.brokerIp, data=news.to_json())
+        conn = Connection();
+        conn.send_data(port=self.port_broker,  ip=self.ip_broker, data=msg.to_json())
 
     def to_json(self):
         return json.dumps(self.__dict__)
@@ -31,7 +32,7 @@ class Publisher:
 
 news = Noticia(body='Test Body',
                 author='El Tiempo',
-                category=[NewsCategory.INUNDACIONES, NewsCategory.INCENDIOS])
-p = Publisher(brokerIp='192.168.0.113', brokerPort=5001, nombre='El Tiempo')
-p.subscribe()
+                temas=[NewsCategory.INCENDIOS])
+p = Publisher(ip_broker='192.168.0.113', port_broker=5001, nombre='El Tiempo')
+# p.subscribe()
 p.send_news(news)
