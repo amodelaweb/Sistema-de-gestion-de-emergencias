23394abba70b62e73e75a8cd721b6a58c3221bc8
Juan Pablo Peñaloza Botero <juanpablo@Juans-MacBook.local>
2018-09-17 23:58:10 -0500

    Fixed Bugs

--- Broker/Modelo/Broker.py
+++ Broker/Modelo/Broker.py
@@ -48,6 +48,7 @@ class Broker(object):
                 print(element)
                 if element in data.get('body').get('temas'):
                     self.conection.send_data(client.puerto, client.ip , data)
+                    return None
                     #break
                 #Fi
             #Rof
